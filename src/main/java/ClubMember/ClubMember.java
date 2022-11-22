b343525ecc9f1d01f5c0ee3480bd25248f303b69
marcusFN <marc599n@stud.kea.dk>
2022-11-22 21:03:30 +0100

    added constructer and with setter and getter in Clubmember class 2.0

--- src/main/java/ClubMember/ClubMember.java
+++ src/main/java/ClubMember/ClubMember.java
@@ -1,46 +1,46 @@
 package ClubMember;
 
 public class ClubMember {
+    private String name;
     private int age;
     private String activityStatus;
     private String membershipType;
     private String swimType;
 
-    public ClubMember(int age, String activityStatus, String membershipType, String swimType){
+    public ClubMember(String name, int age, String activityStatus, String membershipType, String swimType){
+        this.name = name;
         this.age = age;
         this.activityStatus = activityStatus;
         this.membershipType = membershipType;
         this.swimType = swimType;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public String getActivityStatus() {
         return activityStatus;
     }
-
     public void setActivityStatus(String activityStatus) {
         this.activityStatus = activityStatus;
     }
-
     public String getMembershipType() {
         return membershipType;
     }
-
     public void setMembershipType(String membershipType) {
         this.membershipType = membershipType;
     }
-
     public String getSwimType() {
         return swimType;
     }
-
     public void setSwimType(String swimType) {
         this.swimType = swimType;
     }
