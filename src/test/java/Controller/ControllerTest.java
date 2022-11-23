9633d7817330dc031d88dab237a07f104c5afae5
Chrilleweb <96743881+Chrilleweb@users.noreply.github.com>
2022-11-23 19:31:37 +0100

    Controller tests

--- src/test/java/Controller/ControllerTest.java
+++ src/test/java/Controller/ControllerTest.java
@@ -1,6 +1,9 @@
 package Controller;
 
+import DataSouce.Database;
 import org.junit.jupiter.api.Test;
+
+import javax.xml.crypto.Data;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,25 +11,69 @@ class ControllerTest {
 
     @Test
     void createClubMember() {
+        //Arrange
+        Database database = new Database();
+        //Act
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+
+
+        int actual = database.getClubMembers().size();
+        int expected = 14;
+        //Assert
+        assertEquals(actual, expected);
     }
 
     @Test
     void getClubMembers() {
+        //Arrange
+        Database database = new Database();
+        database.createClubMember("JN123523", "Christian", 22, "j", "senior", "exerciser");
+        //Act
+        database.getClubMembers();
+        int actual = database.getClubMembers().size();
+        int expected = 1;
+        //Assert
+        assertEquals(actual, expected);
     }
 
     @Test
     void deleteMember() {
+        //Arrange
+        //Act
+        //Assert
     }
 
     @Test
     void saveData() {
+        //Arrange
+        //Act
+        //Assert
     }
 
     @Test
     void loadData() {
+        //Arrange
+        //Act
+        //Assert
     }
 
     @Test
     void getMembers() {
+        //Arrange
+        //Act
+        //Assert
     }
 }
